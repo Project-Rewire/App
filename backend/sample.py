f32008fc32600e8a1816f840e6629483db4a5cae
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-01-12 19:42:30 +0530

    added sample file to backend

--- backend/sample.py
+++ backend/sample.py
@@ -1,9 +1,3 @@
 """
-Delete this file when backend implementation start
+This ia a dummy file. Delete this file when backend implementation starts
 """
-
-def foo():
-    print("Implement backend off the app in this folder")
-
-if __name__ == "__main__":
-    foo()
