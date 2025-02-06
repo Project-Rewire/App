e42882f6602f694dc85dbce64e0deec653019c42
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-02-06 17:51:00 +0530

    refactored file structure: removed old admin and rebot service

--- backend/admin/rewire_admin/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'f_name', 'l_name', 'b_day']
-        
