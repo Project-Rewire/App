a5dddec7a72dda9d4c69962abc3411de7eb6cc7f
ShakeelGadafi <shakeelgadhafi@gmail.com>
2025-03-02 22:54:58 +0530

    working  with the layers

--- frontend/app/screens/tabs/Home.js
+++ frontend/app/screens/tabs/Home.js
@@ -3,21 +3,26 @@ import { View, StyleSheet, Text } from "react-native";
 import Card from "../../fragments/card"
 import { color } from "@rneui/base";
 
-export default function Home() {
+export default function HomeScreen() {
     return (
         <View style={{ padding: 8 }}>
             <Card onPress={() => console.log("Card Pressed")}>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Overall Progress</Card.Title>
                 <Card.Content>
-                    <Text>This is some card content with default styling.</Text>
+                    <Text>48% - Good</Text>
+                </Card.Content>
+            </Card>
+            <Card onPress={() => console.log("Daily Task Pressed")}>
+                <Card.Title>Daily Task</Card.Title>
+                <Card.Content>
+                    <Text>65% - Almost There</Text>
                 </Card.Content>
             </Card>
             <Card style={{ backgroundColor: "#ee4421", color: "#fff", borderRadius: 32, padding: 32 }}>
-                <Card.Title style={{ color: "#eee" }}>Custom Styled Card</Card.Title>
+                <Card.Title style={{ color: "#eee" }}>Achievements</Card.Title>
                 <Card.Content>
-                    <Text>
-                        You can override default styles by passing custom style props.
-                    </Text>
+                    <Text>Task 07 Completed - Day 7 Gold</Text>
+                    <Text>7 Days Streak - 1st Streak</Text>
                 </Card.Content>
             </Card>
         </View>
