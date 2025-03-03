e45f777ddbc237750e476ee40efcb22a821b0907
Harshana-003 <chamodmalshan03@gmail.com>
2025-03-03 21:18:27 +0530

    added card headings and some styles

--- frontend/app/screens/tabs/Community.js
+++ frontend/app/screens/tabs/Community.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { View, Image, StyleSheet} from "react-native";
+import { View, Image, StyleSheet, Text} from "react-native";
 import Card from "../../fragments/card";
 
 // Main community card(ReWire Community)
 export default function Community() {
     return (
         <View style={{ padding: 10, borderRadius: 20 }}>
+            <Text style={styles.text}>Main Community</Text>
             <Card onPress={() => console.log("Card Pressed")}>
-                <View style = {styles.titleContainer}>
-                    <Image source = {require('../../assets/rewire-logo.png')}
-                    style = {styles.image}/>
+                <View style={styles.titleContainer}>
+                    <Image source={require('../../assets/rewire-logo.png')}
+                        style={styles.image} />
                     <Card.Title>ReWire Community</Card.Title>
                 </View>
             </Card>
+            <Text style={styles.text}>Joined Community</Text>
+            <Text style={styles.text}>Suggestions</Text>
         </View>
     );
 }
@@ -31,6 +34,13 @@ const styles = StyleSheet.create({
         borderRadius: 40,
         borderColor: "green",
         borderWidth: 2,  
+     },
+     text: {
+        color: "grey",
+        fontSize: 14,
+        paddingLeft: 5,
+        paddingBottom: 15,
+        textAlign: "left",
      },
     });
 
