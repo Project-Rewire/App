a00e028731cf1de4d599a4058ef396234dca80ee
Isuru Karunaratna <93091809+isuruK2003@users.noreply.github.com>
2025-03-07 16:18:03 +0530

    chore: aiprofile model creation

--- backend/rewire/aiprofile/models.py
+++ backend/rewire/aiprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+class AIProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    addiction_type  = models.CharField(max_length=30)
+    sentiment_value = models.DecimalField(max_digits=3, decimal_places=3)
+    addiction_duration_months = models.IntegerField()
