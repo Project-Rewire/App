321a493cc54c30ab5cca16431494e04d5a949910
Harshana-003 <chamodmalshan03@gmail.com>
2025-03-17 11:29:39 +0530

    added some missing parts in models

--- backend/rewire/community/models.py
+++ backend/rewire/community/models.py
@@ -1,12 +1,29 @@
+from datetime import timezone
 from django.db import models
 from core.models import User
 
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    icon = models.ImageField(upload_to='community_icons/', null=True, blank=True)
+    category = models.CharField(max_length=100)
     
     def __str__(self):
-        return self.title
+        return self.name
+    @property
+    def member_count(self):
+        return self.members.count()
+    
+class CommunityMember(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='members')
+    joined_at = models.DateTimeField(default=timezone.now)
+    
+    class Meta:
+        unique_together = ('user', 'community')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.community.name}"
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
