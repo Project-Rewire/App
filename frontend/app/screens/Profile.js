585af96aec3c6b016df7e9c907aa97596e3d2b0b
Nidarsa13 <nidarsaragunathan13@gmail.com>
2025-03-22 21:54:04 +0530

    creating a container for the profile page

--- frontend/app/screens/Profile.js
+++ frontend/app/screens/Profile.js
@@ -2,3 +2,6 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
+const Profile = () => {
+  const username = "Andrew Mark";
+
