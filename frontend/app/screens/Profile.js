5d6491876e1e9618955ecfe165e357cb11f7bcbe
Nidarsa13 <nidarsaragunathan13@gmail.com>
2025-03-22 22:13:35 +0530

    styling the user name

--- frontend/app/screens/Profile.js
+++ frontend/app/screens/Profile.js
@@ -76,6 +76,13 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  username: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginLeft: 16,
+  },
+  
 
 
 
