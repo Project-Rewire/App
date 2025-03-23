8d0f8492f51c3993bc50c3fed361d9d6876def34
Harshana-003 <chamodmalshan03@gmail.com>
2025-03-23 20:48:31 +0530

    make some changes in models.py

--- backend/rewire/community/models.py
+++ backend/rewire/community/models.py
@@ -4,9 +4,12 @@ from django.utils import timezone
 
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
+    description = models.TextField()
     name = models.CharField(max_length=255)
     icon = models.ImageField(upload_to='community_icons/', null=True, blank=True)
     category = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_communities')
+    created_at = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return self.name
@@ -14,13 +17,15 @@ class Community(models.Model):
     def member_count(self):
         return self.members.count()
     
-class CommunityMember(models.Model):
+class CommunityMembership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='members')
     joined_at = models.DateTimeField(default=timezone.now) # returns utc time by default
     
     class Meta:
-        unique_together = ('user', 'community')
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'community'], name='unique_user_community_allocation')
+        ]
 
     def __str__(self):
         return f"{self.user.username} - {self.community.name}"
