68c548b9ac191171036d7336d836d17ff0dc626e
Harshana-003 <chamodmalshan03@gmail.com>
2025-03-24 08:25:13 +0530

    done some changes to the code

--- backend/rewire/community/admin.py
+++ backend/rewire/community/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Community, CommunityMembership, Post, CommunityAllocation
 
-admin.site.register(Community)
-admin.site.register(CommunityMembership)
-admin.site.register(Post)
-admin.site.register(CommunityAllocation)
+admin.register(Community)
+admin.register(CommunityMembership)
+admin.register(Post)
+admin.register(CommunityAllocation)
 
 
